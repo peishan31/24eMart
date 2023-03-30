e0be1f5685e71747386e26b128ffa99d3351f726
zhiyi456 <zhiyi456@gmail.com>
2023-03-31 02:44:46 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -27,7 +27,7 @@ app.register_blueprint(admin)
 # stripe.api_key = os.environ["STRIPE_PRIVATE"]
 # TODO: set up environment variables in the future
 app.config["SECRET_KEY"] = "123" # TODO: research on what this secret key is for
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://admin:sharedCMEAccess@mysqldb.csxucthsan5l.ap-southeast-1.rds.amazonaws.com:3306/mysqldb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://admin:sharedCMEAccess@rdspublic.csxucthsan5l.ap-southeast-1.rds.amazonaws.com:3306/rdspublic"
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost:3306/24emart'
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
